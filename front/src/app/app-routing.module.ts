ea39e4f43c57f33356bf33655bd4e9064d08818c
BandaweJ <bands500@gmail.com>
2022-01-15 16:36:46 +0200

    done navigation

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewComponent } from './new/new.component';
+import { TrackComponent } from './track/track.component';
+import { ManageComponent } from './manage/manage.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'new', component: NewComponent },
+  { path: 'track', component: TrackComponent },
+  { path: 'manage', component: ManageComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '',   redirectTo: 'new', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: NewComponent },//Wildcard route for a 404 page
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NewComponent } from './new/new.component';
+import { TrackComponent } from './track/track.component';
+import { ManageComponent } from './manage/manage.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NewComponent,
+    TrackComponent,
+    ManageComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
