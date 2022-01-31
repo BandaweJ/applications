d73acd70524a57e51696403ce869128e84ab57c9
bandawej <bands500@gmail.com>
2022-01-31 21:36:27 +0200

    saving

--- back/src/models/read-school-dto.ts
+++ back/src/models/read-school-dto.ts
@@ -1,18 +1,13 @@
 import { IsEmail, IsString } from 'class-validator';
 
 export class ReadSchoolDto {
-  @IsString()
   id: string;
 
-  @IsString()
   name: string;
 
-  @IsString()
   cell: string;
 
-  @IsEmail()
   email: string;
 
-  @IsString()
   address: string;
 }
